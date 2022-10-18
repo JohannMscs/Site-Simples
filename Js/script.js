c578873e53994c54d9296962a7d967d6233199e7
JohannMscs <johann.moscoso@gmail.com>
2022-10-17 23:16:23 -0300

    alterações no Js

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+
+//console.log("ola mundo")
+
+//let nome = "Jose Alberto"
+//let idade = 30
+//let cidade = "Santa Catarina"
+//console.log(nome, idade, cidade)
+
+//arrays (vetores)
+//let notas = [10, 8.5, 8.4, 3.0]
+//for(let i =0; i<4; i++){
+//    console.log(notas[i])
+//}
+//console.log(notas)
+
+//let aluno = {
+   // nome: "Jose Alberto",
+   // idade: 18,
+   // nota:[10, 8, 7.5, 8.7]
+//}
+//console.log(aluno)
