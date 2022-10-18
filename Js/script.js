add032192b49c5ddfc59136708253cd3d57d0339
JohannMscs <johann.moscoso@gmail.com>
2022-10-17 23:58:33 -0300

    terminando o Javascript

--- Js/script.js
+++ Js/script.js
@@ -1,21 +1,11 @@
-
-//console.log("ola mundo")
-
-//let nome = "Jose Alberto"
-//let idade = 30
-//let cidade = "Santa Catarina"
-//console.log(nome, idade, cidade)
-
-//arrays (vetores)
-//let notas = [10, 8.5, 8.4, 3.0]
-//for(let i =0; i<4; i++){
-//    console.log(notas[i])
-//}
-//console.log(notas)
-
-//let aluno = {
-   // nome: "Jose Alberto",
-   // idade: 18,
-   // nota:[10, 8, 7.5, 8.7]
-//}
-//console.log(aluno)
+let teacherForms = document.getElementsByClassName("teacher-contract")
+for (let teacherForm of teacherForms){
+    teacherForm.addEventListener("submit", function(event){
+        event.preventDefault()
+       let price = teacherForm.price.value
+       let hours = teacherForm.hours.value
+       let total = price * hours
+       console.log(total)
+       alert("R$"+total)
+})
+}
